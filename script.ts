2d23092105ead8b4b3df7cda480baeeca74f4641
Ismat Fatima <159720166+Fatima-Ismat@users.noreply.github.com>
2024-09-11 14:56:29 +0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,6 @@
+document.getElementById('toggleSkills')?.addEventListener('click', () => {
+     const skillsSection = document.getElementById('skills');
+     if (skillsSection) {
+         skillsSection.style.display = skillsSection.style.display === 'none' ? 'block' : 'none';
+     }
+ });
